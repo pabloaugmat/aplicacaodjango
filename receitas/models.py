8e55dec5002e066c38b1fddc143fb6b6b49fcbf9
Pablo Augusto <pabloaugmat@gmail.com>
2021-03-27 14:29:26 -0300

    adicao da funcionalidade de exibir fotos da receita

--- receitas/models.py
+++ receitas/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from datetime import datetime
+from pessoas.models import Pessoa
 
 class Receita(models.Model):
+	pessoa = models.ForeignKey(Pessoa, on_delete = models.CASCADE)
 	nome_receita = models.CharField(max_length = 200)
 	ingredientes = models.TextField()
 	modo_preparo = models.TextField()
@@ -9,4 +11,5 @@ class Receita(models.Model):
 	rendimento = models.CharField(max_length = 100)
 	categoria = models.CharField(max_length = 100)
 	date_receita = models.DateTimeField(default = datetime.now, blank = True)
-
+	foto_receita = models.ImageField(upload_to = 'fotos/%d/%m/%Y', blank= True)
+	publicada = models.BooleanField(default=False)
