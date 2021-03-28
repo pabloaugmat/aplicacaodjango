eae0c8267cd1927ffa275a62840ebc1fd60f4b3b
Pablo Augusto <pabloaugmat@gmail.com>
2021-03-28 03:00:36 -0300

    alteração no buscar

--- receitas/views.py
+++ receitas/views.py
@@ -21,6 +21,21 @@ def receita(request, receita_id):
 		'receita' : receita
 	}
 
-	return render(request, 'receita.html', receita_a_exibir)	
+	return render(request, 'receita.html', receita_a_exibir)
+
+def buscar(request):
+
+	lista_receitas = Receita.objects.order_by('-date_receita').filter(publicada = True)
+
+	if 'buscar' in request.GET:
+		nome_a_buscar = request.GET['buscar']
+		if nome_a_buscar:
+			lista_receitas = lista_receitas.filter(nome_receita__icontains = nome_a_buscar)
+
+	dados = {
+		'receitas' : lista_receitas
+	}
+
+	return render(request, 'buscar.html', dados)
 
 
